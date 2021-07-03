116774188e3dc9c4a35412dcf396ce676d1d2aea
XantX <hp_xantx@hotmail.com>
2021-07-02 22:31:01 -0500

    feat: reponses
    add payment, schedule, student and tutor response

---
+++ Domain/Services/Communications/ScheduleResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Gappstone.API.Domain.Models;
+
+namespace Gappstone.API.Domain.Services.Communications
+{
+    public class ScheduleResponse : BaseResponse<ScheduleResponse>
+    {
+        public ScheduleResponse(ScheduleResponse resource) : base(resource)
+        {
+        }
+
+        public ScheduleResponse(string message) : base(message)
+        {
+        }
+    }
+}
